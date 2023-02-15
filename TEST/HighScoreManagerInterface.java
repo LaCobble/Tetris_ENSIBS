1997d8eafa15f0f09370d542371fbb9f776b979c
LaCobble <maucourt.e2203844@etud.univ-ubs.fr>
2023-02-15 09:17:26 +0100

    Interface

---
+++ TEST/HighScoreManagerInterface.java
@@ -0,0 +1,5 @@
+interface HighScoreManagerInterface {
+    public void addScore(String name, int score);
+    public int getHighScore();
+    public void clearHighScores();
+}
---
+++ TEST/MenuInterface.java
@@ -0,0 +1,9 @@
+interface HighScoreManagerInterface {
+    public void show();
+    public hide();
+    public void startGame();
+    public void displayHighScores();
+    public void displayOptions();
+    public void displayCredits();
+    public void quitGame();
+}
