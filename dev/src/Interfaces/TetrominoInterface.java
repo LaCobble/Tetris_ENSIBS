3c2fd9d13f24e198a39df2fa9129b99a6be43e72
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2023-02-16 09:08:40 +0100

    Update TetrominoInterface.java

--- dev/src/Interfaces/TetrominoInterface.java
+++ dev/src/Interfaces/TetrominoInterface.java
@@ -1,4 +1,28 @@
 package Interfaces;
 
 public interface TetrominoInterface {
+
+    boolean canMoveLeft();
+
+    boolean canMoveRight();
+
+    boolean canRotate();
+
+    boolean canFall();
+
+    void moveLeft();
+
+    void moveRight();
+
+    void rotate();
+
+    void fall();
+
+    void getX();
+
+    void getY();
+
+    TetrominoTypeInterface getTetrominoType();
+
+    int getRotation();
 }
