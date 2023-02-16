2b1b8899ce5bf915c749f7c62039b2ffd322bab5
Warzia1 <latour.morgan21@gmail.com>
2023-02-16 09:32:02 +0100

    setup interface

--- dev/src/Interfaces/BlockInterface.java
+++ dev/src/Interfaces/BlockInterface.java
@@ -1,4 +1,7 @@
 package Interfaces;
 
+import java.awt.*;
+
 public interface BlockInterface {
+    Color getColor();
 }
--- dev/src/Interfaces/MenuInterface.java
+++ dev/src/Interfaces/MenuInterface.java
@@ -1,4 +1,9 @@
 package Interfaces;
 
 public interface MenuInterface {
+    void show();
+    void hide();
+    void startGame();
+    void displayHighScore();
+
 }
