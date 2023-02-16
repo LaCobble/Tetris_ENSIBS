afc158f0c2178f80e64629b54e2d91ddb887e882
BNA-02 <60223574+BNA-02@users.noreply.github.com>
2023-02-16 09:14:32 +0100

    Update Point.java

--- dev/src/Point.java
+++ dev/src/Point.java
@@ -1,2 +1,17 @@
 public class Point {
+    private int x;
+    private int y;
+
+    public Point(int a, int b){
+        this.x=a;
+        this.y=b;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
 }
