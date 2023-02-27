e9d7671dd4557795cbb850c3947183c0dd35b1f0
Warzia1 <latour.morgan21@gmail.com>
2023-02-27 10:56:22 +0100

    creation Block

--- dev/src/Block.java
+++ dev/src/Block.java
@@ -1,3 +1,12 @@
+import java.awt.Color;
 public class Block {
+    public Color color;
 
+    public void setColor(Color color){
+        this.color = color;
+    }
+    public Color getColor(){
+        return color;
+    }
 }
+
