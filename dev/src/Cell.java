710dd567e38986076c3d4cf9739ea56d1cd1a335
Warzia1 <latour.morgan21@gmail.com>
2023-02-27 10:51:59 +0100

    creation class cell

--- dev/src/Cell.java
+++ dev/src/Cell.java
@@ -1,2 +1,19 @@
+import java.awt.Color;
 public class Cell {
+    public Boolean occupied = false;
+    public Color color;
+
+    public Color getColor(){
+        return color;
+    }
+    public void setOccupied(Boolean occupied){
+        this.occupied = occupied;
+    }
+    public Boolean getState(){
+        return  occupied;
+    }
+    public void setColor(Color color){
+        this.color=color;
+    }
+
 }
