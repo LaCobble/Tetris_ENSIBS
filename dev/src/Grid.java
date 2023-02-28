8873c0d27d2b6f557d9af85a09633fdbc34766b0
Warzia1 <latour.morgan21@gmail.com>
2023-02-28 15:00:57 +0100

    Update Grid.java

--- dev/src/Grid.java
+++ dev/src/Grid.java
@@ -1,2 +1,41 @@
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+
 public class Grid {
+    public Cell[][] cells;
+    private Integer dimension;
+    private Integer x;
+    private Integer y;
+    private TetrominoType typeTetromino;
+
+    public Integer getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Integer dimension) {
+        this.dimension = dimension;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    public TetrominoType getTypeTetromino() {
+        return typeTetromino;
+    }
+
+    public void setTypeTetromino(TetrominoType typeTetromino) {
+        this.typeTetromino = typeTetromino;
+    }
 }
