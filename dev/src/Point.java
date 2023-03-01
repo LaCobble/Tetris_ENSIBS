9eec226798dbf3633cf832652ee06f53636c67ce
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2023-03-01 09:10:25 +0100

    Update Point.java

--- dev/src/Point.java
+++ dev/src/Point.java
@@ -7,6 +7,14 @@ public class Point {
         this.y = y;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public int getX(){
         return this.x;
     }
